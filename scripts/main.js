4d5c029b8bde471bcae9c43b9c5aa05455bad7b6
ElsieMay <elsiemaylawrie1@gmail.com>
2022-02-27 18:38:53 +1100

    added Javescript to nav links, added comments to html

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,18 @@ Modalbtn.addEventListener("click", function () {
 modalclose.addEventListener("click", function () {
 	modalbg.classList.remove("bg-active");
 });
+const navslide = () => {
+	const burger = document.querySelector(".burger");
+	const nav = document.querySelector(".nav_links");
+	const navlinks = document.querySelectorAll(".nav_links li");
+	//Toggle Nav
+	burger.addEventListener("click", function () {
+		nav.classList.toggle("nav-active");
+	});
+	//Animation Links
+	nav_links.forEach((link, index) => {
+		link.style.animation = "navLinkFade 0.5s ease forwards ${index /7s}";
+		console.log(index / 7);
+	});
+};
+navslide();
