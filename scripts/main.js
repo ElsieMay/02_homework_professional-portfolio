e2c89a138619832d58098469739f3d5e613b0249
ElsieMay <elsiemaylawrie1@gmail.com>
2022-04-14 14:44:37 +1000

    added repo and deployed links

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,28 @@ let slidePosition = 0;
 const slides = document.getElementsByClassName("carousel_card");
 const totalSlides = slides.length;
 
+Modalbtn.addEventListener("click", function () {
+	modalbg.classList.add("bg-active");
+});
+modalclose.addEventListener("click", function () {
+	modalbg.classList.remove("bg-active");
+});
+const navslide = () => {
+	const burger = document.querySelector(".burger");
+	const nav = document.querySelector(".nav_links");
+	const nav_links = document.querySelectorAll(".nav_links li");
+	//Toggle Nav
+	burger.addEventListener("click", function () {
+		nav.classList.toggle("nav-active");
+	});
+	//Animation Links
+	nav_links.forEach((link, index) => {
+		link.style.animation = "navLinkFade 0.5s ease forwards ${index /7s}";
+		console.log(index / 7);
+	});
+};
+navslide();
+
 document.getElementById("next").addEventListener("click", function () {
 	moveToNextSlide();
 });
@@ -37,25 +59,3 @@ function moveToPreviousSlide() {
 	}
 	updateSlidePosition();
 }
-
-Modalbtn.addEventListener("click", function () {
-	modalbg.classList.add("bg-active");
-});
-modalclose.addEventListener("click", function () {
-	modalbg.classList.remove("bg-active");
-});
-const navslide = () => {
-	const burger = document.querySelector(".burger");
-	const nav = document.querySelector(".nav_links");
-	const nav_links = document.querySelectorAll(".nav_links li");
-	//Toggle Nav
-	burger.addEventListener("click", function () {
-		nav.classList.toggle("nav-active");
-	});
-	//Animation Links
-	nav_links.forEach((link, index) => {
-		link.style.animation = "navLinkFade 0.5s ease forwards ${index /7s}";
-		console.log(index / 7);
-	});
-};
-navslide();
